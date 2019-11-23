adea6eaa5c940e441b64056c885001fa5427cff5
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2019-11-23 20:37:23 +0900

    :sparkles:  [add] merge

---
+++ merge.rb
@@ -0,0 +1,27 @@
+require 'json'
+
+imgs = []
+Dir.glob('json/*.json').each do |path|
+  File.open(path) do |f|
+    hash = JSON.load(f)
+    path = hash['asset']['path']
+    size = hash['asset']['size']
+    objects = hash['regions'].map do |region|
+      {
+        tag: region['tags'][0],
+        x: region['boundingBox']['left'],
+        y: region['boundingBox']['top'],
+        w: region['boundingBox']['width'],
+        h: region['boundingBox']['height']
+      }
+    end
+    imgs.push({
+      path: path,
+      size: size,
+      objects: objects,
+    })
+  end
+end
+File.open('imgs.json', 'w') {|file| 
+  file.puts(JSON.pretty_generate({imgs: imgs}))
+}
